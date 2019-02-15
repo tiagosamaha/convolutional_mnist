5075145bf80c1d738dbd63806e0e176107aedcd1
Tiago Samaha <tiagosamaha@les.inf.puc-rio.br>
2019-02-15 12:33:44 -0200

    adding mnist dataset

---
+++ dataset.py
@@ -0,0 +1,17 @@
+import keras
+from keras.datasets import mnist
+
+num_classes = 10
+img_rows, img_cols = 28, 28
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
