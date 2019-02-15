cd637be1a2aac81034832bd3fd58b50f06cfded5
Tiago Samaha <tiagosamaha@les.inf.puc-rio.br>
2019-02-15 12:50:54 -0200

    training and predicting

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
-from keras.datasets import mnist
+import os
+import keras
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
 
+from keract import get_activations, display_activations
+
+from dataset import x_train, x_test, y_train, y_test
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 batch_size  = 128
-num_classes = 10
 epochs      = 12
 
 layers = [
-    Conv2D(64, (3,3), activation='relu'),
+    Conv2D(64, (3,3), activation='relu', input_shape=(28, 28, 1)),
     MaxPooling2D((2,2)),
     Dropout(0.25),
     Flatten(),
     Dense(128, activation='relu'),
-    Dropout(0.25),
-    Dense(num_classes, activation='softmax')
+    Dropout(0.5),
+    Dense(10, activation='softmax')
 ]
 
 model = Sequential(layers)
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adam(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+
+images = get_activations(model, x_test[:1])
+display_activations(images)
+print('\n####  REPORT  ####')
+print('Input number is =>', y_test[0].argmax())
+print('Output number is =>', model.predict(x_test[:1]).argmax())
